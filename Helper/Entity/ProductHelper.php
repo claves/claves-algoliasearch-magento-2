fca995be4c6dea4825eed00c413742dfa0dd27e0
Mohit Choudhary <mohit.choudhary@algolia.com>
2023-10-05 16:00:39 +0530

    Added option to select replica type per attribute

--- Helper/Entity/ProductHelper.php
+++ Helper/Entity/ProductHelper.php
@@ -1065,7 +1065,7 @@ class ProductHelper
             }
 
             $type = $product->getTypeId();
-            if ($type !== 'configurable' && $type !== 'grouped' && $type !== 'bundle' && $attributeName != 'test') {
+            if ($type !== 'configurable' && $type !== 'grouped' && $type !== 'bundle') {
                 continue;
             }
 
@@ -1228,7 +1228,7 @@ class ProductHelper
             $value = $attributeResource->getFrontend()->getValue($product);
         }
 
-        if ($value !== null) {
+        if ($value) {
             $customData[$attribute['attribute']] = $value;
         }
 
